c59bac0f502658f0b21427cde8ff51b1aab2e7a3
khushi667 <khushi.mis03@gmail.com>
2025-06-14 19:01:06 +0530

    learn datatypes in TypeScript

---
+++ app.js
@@ -0,0 +1,19 @@
+var userName = 'khushi';
+var age = 21;
+var college = 'Agrawal College';
+console.log(userName);
+console.log(age);
+console.log(college);
+// categories of datatype--> Primitive Types(number,string,boolean,null, undefined,bigint,symbol), Object Types(array,tuple,object), special Types,Advance tTypes, Function Types 
+var num = 21; //number
+var num = 21.8; // in javascript there is no float datatype we can write it in number
+var isLogic = false; // boolean
+var data = ""; // null
+var a = undefined; // undefined
+// symbol is special type of Primitivedata type who generate unique memory address.
+// const sym1: symbol = Symbol();
+// const sym2: symbol = Symbol('description');
+// const sym3: symbol = Symbol('description');
+// console.log(sym1 === sym2); // false
+// console.log(sym2 === sym3); // false
+var numArray = [10, 20, 30]; // array
---
+++ app.ts
@@ -0,0 +1,33 @@
+var userName:string = 'khushi'
+var age:Number = 21
+var college = 'Agrawal College'
+
+console.log(userName)
+console.log(age)
+console.log(college)
+
+
+
+// categories of datatype--> Primitive Types(number,string,boolean,null, undefined,bigint,symbol), Object Types(array,tuple,object), special Types,Advance tTypes, Function Types 
+
+
+var num:number = 21;   //number
+var num:number = 21.8;  // in javascript there is no float datatype we can write it in number
+
+var isLogic = false;  // boolean
+
+var data = "";   // null
+
+var a:undefined = undefined  // undefined
+
+// symbol is special type of Primitivedata type who generate unique memory address.
+// const sym1: symbol = Symbol();
+// const sym2: symbol = Symbol('description');
+// const sym3: symbol = Symbol('description');
+
+// console.log(sym1 === sym2); // false
+// console.log(sym2 === sym3); // false
+
+
+
+var numArray:Number[] = [10,20,30]  // array
